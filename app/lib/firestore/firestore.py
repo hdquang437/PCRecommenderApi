dc7f1d1610b72b88cb8af0b9a755ee71316bedb9
Huynh Dang Quang <hdquang437@gmail.com>
2025-06-10 22:56:53 +0700

    Update firestore.py

--- app/lib/firestore/firestore.py
+++ app/lib/firestore/firestore.py
@@ -1,8 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 
 # Load Firebase credentials
-cred = credentials.Certificate("./firebase_key.json")
+
+firebase_cred_json = os.getenv("FIREBASE_KEY_JSON")
+cred_dict = json.loads(firebase_cred_json)
+
+# cred = credentials.Certificate("./firebase_key.json")
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
 # Khởi tạo Firestore
